386af7806c43c49baa4650e77022c2a03b04aa19
Lukas Kremsmayr <lukas@kremsmayr.net>
2015-07-14 12:22:02 +0200

    Update AutoShutdownPlugin.php
    added timed shutdown possability

--- plugins/steeffeen/AutoShutdownPlugin.php
+++ plugins/steeffeen/AutoShutdownPlugin.php
@@ -18,17 +18,22 @@ class AutoShutdownPlugin implements Plugin, CallbackListener, TimerListener {
 	/*
 	 * Constants
 	 */
-	const ID                        = 51;
-	const VERSION                   = 0.1;
-	const NAME                      = 'Auto Shutdown Plugin';
-	const AUTHOR                    = 'steeffeen';
-	const SETTING_SHUTDOWN_INTERVAL = 'Shutdown Interval in Days';
-
+	const ID                               = 51;
+	const VERSION                          = 0.2;
+	const NAME                             = 'Auto Shutdown Plugin';
+	const AUTHOR                           = 'steeffeen';
+	const SETTING_SHUTDOWN_INTERVAL_ENABLE = 'Enable Interval Shutdown';
+	const SETTING_SHUTDOWN_INTERVAL        = 'Shutdown Interval in Days';
+	const SETTING_TIMED_SHUTDOWN           = ' Enable timed Shutdown';
+	const SETTING_TIMED_SHUTDOWN_DAY       = ' Weekday of timed Shutdown';
+	const SETTING_TIMED_SHUTDOWN_HOUR      = ' Hour of timed Shutdown';
+	
 	/**
 	 * Private Properties
 	 */
 	/** @var ManiaControl $maniaControl */
-	private $maniaControl = null;
+	private $maniaControl      = null;
+	private $shutdownRequested = false;
 
 	/**
 	 * @see \ManiaControl\Plugins\Plugin::getId()
@@ -78,11 +83,15 @@ class AutoShutdownPlugin implements Plugin, CallbackListener, TimerListener {
 		$this->maniaControl = $maniaControl;
 
 		// Init settings
-		$this->maniaControl->settingManager->initSetting($this, self::SETTING_SHUTDOWN_INTERVAL, 7);
+		$this->maniaControl->getSettingManager()->initSetting($this, self::SETTING_SHUTDOWN_INTERVAL_ENABLE, true);
+		$this->maniaControl->getSettingManager()->initSetting($this, self::SETTING_SHUTDOWN_INTERVAL, 7);
+		$this->maniaControl->getSettingManager()->initSetting($this, self::SETTING_TIMED_SHUTDOWN, false);
+		$this->maniaControl->getSettingManager()->initSetting($this, self::SETTING_TIMED_SHUTDOWN_DAY, array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+		$this->maniaControl->getSettingManager()->initSetting($this, self::SETTING_TIMED_SHUTDOWN_HOUR, array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23));
 
 		// Register callbacks
-		$this->maniaControl->callbackManager->registerCallbackListener(PlayerManager::CB_SERVER_EMPTY, $this, 'handleServerEmptyCallback');
-		$this->maniaControl->timerManager->registerTimerListening($this, 'handle12HoursTimerCallback', 12 * 3600 * 1000);
+		$this->maniaControl->getCallbackManager()->registerCallbackListener(PlayerManager::CB_SERVER_EMPTY, $this, 'handleServerEmptyCallback');
+		$this->maniaControl->getTimerManager()->registerTimerListening($this, 'handleHourlyTimerCallback', 3600 * 1000);
 	}
 
 	/**
@@ -99,9 +108,9 @@ class AutoShutdownPlugin implements Plugin, CallbackListener, TimerListener {
 	}
 
 	/**
-	 * Handle Each12Hours Timer Callback
+	 * Handle Each 1 Hour Timer Callback
 	 */
-	public function handle12HoursTimerCallback() {
+	public function handleHourlyTimerCallback() {
 		$this->checkShutdown();
 	}
 
@@ -109,23 +118,56 @@ class AutoShutdownPlugin implements Plugin, CallbackListener, TimerListener {
 	 * Check if the Server should be shut down and perform it if necessary
 	 */
 	private function checkShutdown() {
-		// Check if server is empty
-		if (!$this->maniaControl->server->isEmpty()) {
-			return;
+		$intervalShutdownEnabled = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_SHUTDOWN_INTERVAL_ENABLE);
+		if ($intervalShutdownEnabled) {
+			// Check if server is empty
+			if (!$this->maniaControl->getServer()->isEmpty()) {
+				return;
+			}
+
+
+			// Check if shutdown interval is reached
+			$networkStats         = $this->maniaControl->getClient()->getNetworkStats();
+			$uptimeDays           = $networkStats->uptime / (24 * 3600);
+			$shutdownIntervalDays = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_SHUTDOWN_INTERVAL);
+			if ($shutdownIntervalDays <= 0 || $uptimeDays < $shutdownIntervalDays) {
+				return;
+			}
+
+			// Shut down server
+			$this->maniaControl->getClient()->stopServer();
+
+			// Quit ManiaControl
+			$this->maniaControl->quit("AutoShutdownPlugin: Shut down Server after {$shutdownIntervalDays} Days.");
 		}
 
-		// Check if shutdown interval is reached
-		$networkStats         = $this->maniaControl->client->getNetworkStats();
-		$uptimeDays           = $networkStats->uptime / (24 * 3600);
-		$shutdownIntervalDays = $this->maniaControl->settingManager->getSettingValue($this, self::SETTING_SHUTDOWN_INTERVAL);
-		if ($shutdownIntervalDays <= 0 || $uptimeDays < $shutdownIntervalDays) {
-			return;
+
+		$timedShutdownEnabled = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_TIMED_SHUTDOWN);
+		if ($timedShutdownEnabled) {
+			$day  = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_TIMED_SHUTDOWN_DAY);
+			$hour = $this->maniaControl->getSettingManager()->getSettingValue($this, self::SETTING_TIMED_SHUTDOWN_HOUR);
+
+
+			$networkStats = $this->maniaControl->getClient()->getNetworkStats();
+			$uptimeHours  = $networkStats->uptime / (3600);
+
+			// Check if server is running more than one hour, weekday, and time
+			if ($uptimeHours > 1 && $day == date('l', time()) && intval($hour) == intval(date('G', time()))) {
+				$this->shutdownRequested = true;
+			}
 		}
 
-		// Shut down server
-		$this->maniaControl->client->stopServer();
+		if ($this->shutdownRequested) {
+			// Check if server is empty
+			if (!$this->maniaControl->getServer()->isEmpty()) {
+				return;
+			}
 
-		// Quit ManiaControl
-		$this->maniaControl->quit("AutoShutdownPlugin: Shut down Server after {$shutdownIntervalDays} Days.");
+			// Shut down server
+			$this->maniaControl->getClient()->stopServer();
+
+			// Quit ManiaControl
+			$this->maniaControl->quit("AutoShutdownPlugin: Shut down Server after Time Request.");
+		}
 	}
 }
